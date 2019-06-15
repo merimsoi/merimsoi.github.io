b3e28a12d770bbb7e2086db79020c42f873ff2b9
merimsoi <merimson@hotmail.com>
2019-06-15 09:26:13 -0500

    commit from git gui

--- templates/mvcAndReact/js/src/App.js
+++ templates/mvcAndReact/js/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! original!!!</h1>
+        <h1> Hello, World! original2!!!</h1>
       </div>
     );
   }
