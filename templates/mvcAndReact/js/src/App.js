6e12934f25a75ca71c9e7d7da7b25031f09bc025
merimsoi <merimson@hotmail.com>
2019-06-15 09:25:07 -0500

    commit from root folder

--- templates/mvcAndReact/js/src/App.js
+++ templates/mvcAndReact/js/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! WOW test changes!!!</h1>
+        <h1> Hello, World! original!!!</h1>
       </div>
     );
   }
