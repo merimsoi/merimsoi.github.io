09b31c921fe509260270c4260061d5b8289c635b
merimsoi <merimson@hotmail.com>
2019-06-15 09:42:32 -0500

    some test

--- templates/mvcAndReact/js/src/App.js
+++ templates/mvcAndReact/js/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! WOW test changes from mvcReact!!!</h1>
+        <h1> Hello, World! clean now</h1>
       </div>
     );
   }
