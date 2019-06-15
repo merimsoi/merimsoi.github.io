287ef01d78bddd03b1c7cf641537b40e10b769de
merimsoi <merimson@hotmail.com>
2019-06-15 09:20:03 -0500

    more in sync' exit

--- templates/mvcAndReact/js/src/App.js
+++ templates/mvcAndReact/js/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! WOW my root app-react finally works!!!</h1>
+        <h1> Hello, World! WOW test changes!!!</h1>
       </div>
     );
   }
