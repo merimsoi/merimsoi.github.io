5ce6e08286b871fad282bfeed33c6fe9d27b9a60
merimsoi <merimson@hotmail.com>
2019-06-15 09:36:34 -0500

    2

--- templates/mvcAndReact/js/src/App.js
+++ templates/mvcAndReact/js/src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! original2!!!</h1>
+        <h1> Hello, World! WOW test changes!!!</h1>
       </div>
     );
   }
